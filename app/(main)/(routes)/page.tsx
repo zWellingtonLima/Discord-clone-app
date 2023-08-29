98f067e4cbe6ed16d764893e06aa6ad8af474a2a
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-08-29 17:21:06 +0100

    Feat: added authentication using clerk

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>This is a protected route.</div>
+}
+
+export default Home
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const open = Open_Sans({ subsets: ['latin'] })
+const open = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Team Chat Application',
-  description: 'This is a discord clone app made using NextJs13',
-}
+  title: "Team Chat Application",
+  description: "This is a discord clone app made using NextJs13",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={open.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={open.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
