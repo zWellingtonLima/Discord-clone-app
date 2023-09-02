6e9bc3e7dde4a3adb1ff3f70bd441f9734f5cf7a
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-09-02 10:31:32 +0100

    Feat: navigation sidebar step render server and added basic styles

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,10 +1,13 @@
 import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import NavigationAction from "./navigation-action";
+import NavigationItem from "./navigation-item";
+import { ModeToggle } from "../mode-toggle";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -29,11 +32,17 @@ export const NavigationSidebar = async () => {
       <Separator className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto'/>
       <ScrollArea className='flex-1 w-full'>
         {servers.map((server) => (
-          <div key={server.id}>
-            {server.name}
+          <div key={server.id} className='mb-4'>
+            <NavigationItem id={server.id} name={server.name} imageUrl={server.imageUrl}/>
           </div>
         ))}
       </ScrollArea>
+      <div className='pb-3 mt-auto flex items-center flex-col gap-y-4'>
+        <ModeToggle />
+        <UserButton afterSignOutUrl="/" appearance={{elements: {
+          avatarBox: "h-[48px] w-[48px]"
+        }}}/>
+      </div>
     </div>
   );
 };
