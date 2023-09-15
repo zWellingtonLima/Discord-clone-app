e3a507ddf4a9d2903d77be365ab2b952d8d3e44d
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-09-15 12:48:46 +0100

    Feat: channel creation added

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -70,7 +70,10 @@ const ServerHeader: FC<ServerHeaderProps> = ({ server, role }) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("createChannel", { server })}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
