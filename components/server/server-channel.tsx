52464d5516b1d56a74e0e8a6611f51140b1d2f9e
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-09-17 08:15:18 +0100

    Fix: quick fix to light color sidebard nagivation and "delete" string to trash icons on serverChannel

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -51,7 +51,7 @@ export const ServerChannel = ({
           <ActionTooltip label="Edit">
             <Edit className="hidden group-hover:block text-zinc-500 w-4 h-4 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
           </ActionTooltip>
-          <ActionTooltip label="Edit">
+          <ActionTooltip label="Delete">
             <Trash className="hidden group-hover:block text-zinc-500 w-4 h-4 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
           </ActionTooltip>
         </div>
