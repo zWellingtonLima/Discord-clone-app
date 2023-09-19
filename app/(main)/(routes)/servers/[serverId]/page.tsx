73bacb905139c49cc8ba1bdcd27de94805b23678
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-09-19 17:22:21 +0100

    Feat: Channel Id Page done

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,50 @@
-const ServerIdPage = () => {
-  return <div>ServerId Page</div>
+import { redirectToSignIn } from "@clerk/nextjs";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
 }
 
-export default ServerIdPage
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  } // This should never happens but just in case returns null.
+
+  return redirect(`/server/${params.serverId}/channels/${initialChannel?.id}`);
+};
+
+export default ServerIdPage;
