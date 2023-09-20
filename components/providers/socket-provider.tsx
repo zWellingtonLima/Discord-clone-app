916872d1f8f8cf5948326ccb777b2daeb93f82c3
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-09-20 19:12:02 +0100

    Feat: socket Provider

---
+++ components/providers/socket-provider.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useState, createContext, useContext } from "react";
+import { io as ClientIO } from "socket.io-client";
+
+type SocketContextType = {
+  socket: any | null;
+  isConnected: boolean;
+};
+
+const SocketContext = createContext<SocketContextType>({
+  socket: null,
+  isConnected: false,
+});
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+  const [socket, setSocket] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    const socketInstance = new (ClientIO as any)(
+      process.env.NEXT_PUBLIC_SITE_URL!,
+      {
+        path: "/api/socket/io",
+        addTrailingSlash: false,
+      }
+    ); // This is, by default, our localHost in development. When I deploy I have to change this NEXT_PUBLIC_SITE_URL to match my deploy URL.
+
+    socketInstance.on("connect", () => {
+      setIsConnected(true);
+    });
+
+    socketInstance.on("disconnect", () => {
+      setIsConnected(false);
+    });
+
+    setSocket(socketInstance);
+
+    return () => {
+      socketInstance.disconnect();
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket, isConnected }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
