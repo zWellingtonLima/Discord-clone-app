6f7dc9882c69ba758723f83c45f32bcfd17e6ea0
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-12-04 12:27:01 +0000

    fix: apiroute

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,6 @@ export default authMiddleware({
 });
  
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)", "/api/socket/io"],
 };
  
